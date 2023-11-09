4197a8eeb5feabd37a89d6a23dd55c96127d8e67
Kevin-Stephens <kmstephens437@gmail.com>
2023-11-09 14:23:55 -0600

    Category.js Model

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,20 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // defining id column
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      }
+
+    //defining category_name column
+      category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+  
   },
   {
     sequelize,
