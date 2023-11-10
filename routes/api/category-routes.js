0ca074cebdae0c41e175d78732f0cd7ca2d91f6a
Kevin-Stephens <kmstephens437@gmail.com>
2023-11-09 18:30:10 -0600

    post route for new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,8 +38,15 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  // creating a new category
 });
+try {
+  const categoryInfo = Category.create(req.body);
+  res.status(200).json(categoryInfo);
+}
+catch (err) {
+  res.status(400).json(err);
+}
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
